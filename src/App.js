2420cebe1e81af7eb72cb3fa536caa5d2f870d6f
sahil <sahilkeshav1997@gmail.com>
2021-08-30 22:47:13 +0530

    fix: porfolio link updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from "react";
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import $ from 'jquery'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    $(function () {
+      $('.loader').delay(2000).fadeOut('slow')
+      $('#overlayer').delay(2000).fadeOut('slow')
+      setLoading(false)
+    })
+  }, [])
   return (
-    <AuthWrapper>
-      <Router>
-        <Switch>
-          <PrivateRoute path="/" exact>
-            <Dashboard />
-          </PrivateRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
-      </Router>
-    </AuthWrapper>
-  );
+    <>
+      <div id='overlayer'>
+        <span className='loader'>
+          <span className='loader-inner'></span>
+        </span>
+      </div>
+      {!loading && (
+        <AuthWrapper>
+          <Router>
+            <Switch>
+              <PrivateRoute path='/' exact>
+                <Dashboard />
+              </PrivateRoute>
+              <Route path='/login'>
+                <Login />
+              </Route>
+              <Route path='*'>
+                <Error />
+              </Route>
+            </Switch>
+          </Router>
+        </AuthWrapper>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default App
